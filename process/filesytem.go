5125f08db682cb120a26a96844391cd3ddf289da
Suman hazra <sumanhazra368@gmail.com>
2026-07-08 21:22:23 +0530

    add the file system setup next day i wll try to fenished the reamning file sytem work and try to make the project runable

--- process/filesytem.go
+++ process/filesytem.go
@@ -1,1 +1,21 @@
 package process
+
+import (
+	"fmt"
+	"os"
+)
+
+func SetupFilesystem(UUid string) (bool, error) {
+	rootfs := fmt.Sprintf("sandinternal/v1_supersand/%s_rootfs", UUid)
+	if err := os.MkdirAll(rootfs, 0o755); err != nil {
+		return false, err
+	}
+	folders := [8]string{"bin", "dev", "etc", "proc", "sys", "tmp", "usr", "var"}
+	for _, fl := range folders {
+		fls := fmt.Sprintf("%s/%s", rootfs, fl)
+		if erro := os.MkdirAll(fls, 0o755); erro != nil {
+			return false, erro
+		}
+	}
+	return true, nil
+}
